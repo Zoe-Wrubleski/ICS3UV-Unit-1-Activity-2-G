8f5e9ac64529c602b5940721ecb4da5bc462b02f
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 19:15:12 +0000

    using ts to do calculations

--- main.ts
+++ main.ts
@@ -1,4 +1,26 @@
 /**
  * @author Zoe Wrubleski
- * @version
+ * @version 1.0.0
+ * @date 2025-09-30
+ * @fileoverview This program practices using code to solving math problems
  */
+
+//question 1 calculation
+console.log("1. 23 + 8 / 2 = " + (23 + 8 / 2));
+
+//question 2 calculation
+console.log("2. (3 + 8) * 2 = " + ((3 + 8) * 2));
+
+//question 3 calculation
+console.log("3. (8 + 7) * (3 + 6) = " + ((8 + 7) * (3 + 6)));
+
+//question 4 calculation
+console.log("4. 5 + 2 * 7 = " + (5 + 2 * 7));
+
+//question 5 calculation
+console.log("5. My answer to 1 + 33 is " + (1 + 33));
+
+//question 6 calculation
+console.log("6. 6 * (2 + 8) = " + (6 * (2 + 8)));
+
+console.log("\nDone.");
