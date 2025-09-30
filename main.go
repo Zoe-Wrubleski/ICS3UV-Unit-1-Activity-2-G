547e5a27611ef50f1bedf44057d82b03a3f20f53
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-30 19:08:19 +0000

    calculations using go

---
+++ main.go
@@ -0,0 +1,33 @@
+/*
+ *@author
+ *@version
+ *@date
+ *@fileoverview
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//question one calculation
+	fmt.Println("1. 23 + 8 / 2 = " + fmt.Sprint(23 + 8 / 2))
+
+	//question 2 calculation
+	fmt.Println("2. (3 + 8) * 2 = " + fmt.Sprint((3 + 8) * 2))
+
+	//question 3 calculation
+	fmt.Println("3. (8 + 7) * (3 + 6) = " + fmt.Sprint((8 + 7) * (3 + 6)))
+
+	//question 4 calculation
+	fmt.Println("4. 5 + 2 * 7 = " + fmt.Sprint(5 + 2 * 7))
+
+	//question 5 calculation
+	fmt.Println("5. My answer to 1 + 33 is " + fmt.Sprint(1 + 33))
+
+	//question 6 calculation
+	fmt.Println("6. 6 * (2 + 8) = " + fmt.Sprint(6 * (2 + 8)))
+
+	fmt.Println("\nDone.")
+
+}
