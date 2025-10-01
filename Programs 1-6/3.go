a38114c7cbfc83bd686b9f1c8989ad6817afe73a
default <zoe.wrubleski@stu.ocsb.ca>
2025-10-01 11:35:30 +0000

    16 + 23 + 85 + 17 + 21 calculation using go

--- Programs 1-6/3.go
+++ Programs 1-6/3.go
@@ -2,7 +2,7 @@
  *@author Zoe Wrubleski
  *@version 1.0.0
  *@date 2025-10-01
- *@fileoverview 
+ *@fileoverview This program answers the equation 16 + 23 + 85 + 17 + 21.
  */
 
 package main
@@ -10,3 +10,8 @@ package main
 import "fmt"
 
 func main() {
+	//calculation
+	fmt.Println("16 + 23 + 85 + 17 + 21 = " + fmt.Sprint(16 + 23 + 85 + 17 + 21))
+
+	fmt.Println("\nDone.")
+}
