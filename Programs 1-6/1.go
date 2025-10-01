03b4ce10bdd8fb2ac504af944620425b3c336287
default <zoe.wrubleski@stu.ocsb.ca>
2025-10-01 11:27:26 +0000

    17 - 13 using go

---
+++ Programs 1-6/1.go
@@ -0,0 +1,17 @@
+/*
+ *@author Zoe Wrubleski
+ *@version 1.0.0
+ *@date 2025-09-30
+ *@fileoverview This program displays the answer to 17 - 13
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//calculation
+	fmt.Println("17 - 13 = " + fmt.Sprint(17 - 13))
+
+	fmt.Println("\nDone.")
+}
